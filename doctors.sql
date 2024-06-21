c6b132652cfe4d2a52a095511cad03cc7bc839be
Rami S. Jaber <89670401+RamiSJ12@users.noreply.github.com>
2024-06-21 10:10:51 +0800

    Create doctors.sql

---
+++ doctors.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Doctors (
+    DoctorID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Specialization VARCHAR(50),
+    Phone VARCHAR(15),
+    Email VARCHAR(50)
+);
